dc61a194abb6bae832dbced49d47ad8ffae5c301
Alex McGivern <alex@aeonofdiscord.com>
2017-10-22 20:48:49 +0100

    fixed startup error if ~/lu-config/ is missing

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -2,9 +2,12 @@ var lowdb = require('lowdb')
 var FileSyncAdapter = require('lowdb/adapters/FileSync')
 var path = require('path')
 var os = require('os')
+var fs = require('fs')
 
 class Config {
   constructor(self) {
+    var cpath = path.join(os.homedir(), 'lu-config');
+    if(!fs.existsSync(cpath)) { fs.mkdir(cpath); }
     this.db = lowdb(new FileSyncAdapter(path.join(os.homedir(), 'lu-config', 'rotonde-app.json')))
     
     this.db
