02f9055ad9cb7979a724d3c47f13d421f51f9e5d
Jelmer <luxizzle@gmail.com>
2017-10-24 14:26:14 +0200

    it starts?

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,1 +1,11 @@
-console.log('Hello world!')
+import Vue from 'vue'
+
+//import router from './router'
+
+import App from './components/App'
+
+new Vue({
+  template: '<app/>',
+  components: { App },
+  //router
+}).$mount('#app')
